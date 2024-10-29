c04c974cf9eb47f4cb8bed53e8b1995e09d0d757
Ingy döt Net <ingy@ingy.net>
2024-10-29 14:47:18 -0700

    Add a template for update automation

---
+++ template/ys.rb
@@ -0,0 +1,50 @@
+class CLASS < Formula
+  version = 'VERSION'
+
+  sha_lin_arm = 'LIN_ARM'
+  sha_lin_int = 'LIN_INT'
+  sha_mac_arm = 'MAC_ARM'
+  sha_mac_int = 'MAC_INT'
+
+  download = 'https://github.com/yaml/yamlscript/releases/download'
+
+  desc ""
+  homepage "https://github.com/yaml/yamlscript"
+  version version
+  license "MIT"
+
+  on_linux do
+    if Hardware::CPU.arm?
+      url "#{download}/#{version}/ys-#{version}-linux-aarch64.tar.xz"
+      sha256 sha_lin_arm
+    end
+
+    if Hardware::CPU.intel?
+      url "#{download}/#{version}/ys-#{version}-linux-x64.tar.xz"
+      sha256 sha_lin_int
+    end
+  end
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "#{download}/#{version}/ys-#{version}-macos-aarch64.tar.xz"
+      sha256 sha_mac_arm
+    end
+
+    if Hardware::CPU.intel?
+      url "#{download}/#{version}/ys-#{version}-macos-x64.tar.xz"
+      sha256 sha_mac_int
+    end
+  end
+
+  def install
+    bin.install "ys"
+    bin.install "ys-0"
+    bin.install "ys-#{version}"
+  end
+
+  test do
+    assert_equal "YAMLScript #{version}\n",
+      pipe_output("#{bin}/ys --version")
+  end
+end
