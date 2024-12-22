33ac03ba995d90fe23735bba71974e9b011214da
Daniel Bayley <daniel.bayley@me.com>
2024-12-22 00:52:11 +0000

    ys 0.1.87

--- ys.rb
+++ ys.rb
@@ -1,39 +1,34 @@
 class Ys < Formula
-  version = '0.1.86'
-
-  sha_lin_arm = '6a0682546dd3837d9db73624b7d8d0506e9160c5deb10341e3b292afccd30ebb'
-  sha_lin_int = 'c95cbf3a7a1e1fcf5b3da32152ea4ccf019445ed408e7ca40e1a4dcc824618a7'
-  sha_mac_arm = '4fd0ee35d9d73e6c7578f05447a977767116419874af38a10f450221409b36ba'
-  sha_mac_int = 'b028cb93843e0563101b22f36b342f949ac50ed481a95a71c98142a87c46461f'
-
-  download = 'https://github.com/yaml/yamlscript/releases/download'
-
   desc "Program in YAML — Code is Data"
-  homepage "https://github.com/yaml/yamlscript"
-  version version
+  homepage "https://yamlscript.org"
+  version "0.1.87"
   license "MIT"
+  head "https://github.com/yaml/yamlscript.git", branch: "main"
 
-  on_linux do
-    if Hardware::CPU.arm?
-      url "#{download}/#{version}/ys-#{version}-linux-aarch64.tar.xz"
-      sha256 sha_lin_arm
-    end
-
-    if Hardware::CPU.intel?
-      url "#{download}/#{version}/ys-#{version}-linux-x64.tar.xz"
-      sha256 sha_lin_int
-    end
+  livecheck do
+    url :stable
+    strategy :github_latest
   end
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "#{download}/#{version}/ys-#{version}-macos-aarch64.tar.xz"
-      sha256 sha_mac_arm
+    on_arm do
+      url "https://github.com/yaml/yamlscript/releases/download/0.1.86/ys-0.1.86-macos-aarch64.tar.xz"
+      sha256 "4fd0ee35d9d73e6c7578f05447a977767116419874af38a10f450221409b36ba"
     end
+    on_intel do
+      url "https://github.com/yaml/yamlscript/releases/download/0.1.87/ys-0.1.87-macos-x64.tar.xz"
+      sha256 "ac99a7d205a227dd98ccd2fd4daf391a0a3b471792a9eedcebf1efe616e766eb"
+    end
+  end
 
-    if Hardware::CPU.intel?
-      url "#{download}/#{version}/ys-#{version}-macos-x64.tar.xz"
-      sha256 sha_mac_int
+  on_linux do
+    on_arm do
+      url "https://github.com/yaml/yamlscript/releases/download/0.1.86/ys-0.1.86-linux-aarch64.tar.xz"
+      sha256 "6a0682546dd3837d9db73624b7d8d0506e9160c5deb10341e3b292afccd30ebb"
+    end
+    on_intel do
+      url "https://github.com/yaml/yamlscript/releases/download/0.1.86/ys-0.1.86-linux-x64.tar.xz"
+      sha256 "c95cbf3a7a1e1fcf5b3da32152ea4ccf019445ed408e7ca40e1a4dcc824618a7"
     end
   end
 
